005a3aebb8d4709883ad73abbccbfe27e8a6d9b2
alexromanov <al8x.romanov@gmail.com>
2017-02-11 19:48:29 +0200

    Added Weather story

---
+++ src/test/java/serenityrest/jbehave/WeatherApiDefinitions.java
@@ -0,0 +1,14 @@
+package serenityrest.jbehave;
+
+import org.jbehave.core.annotations.When;
+
+/**
+ * Created by aleksandr on 11.02.17.
+ */
+public class WeatherApiDefinitions {
+
+    @When("I call weather api for $city")
+    public void callApiForCurrentWeather(){
+        
+    }
+}
---
+++ src/test/resources/stories/weatherapi/CurrentWeather.story
@@ -0,0 +1,10 @@
+Meta:
+
+Narrative:
+As a user
+I want to perform an action
+So that I can achieve a business goal
+
+Scenario: Get current weather data for Dnipro city
+When I call weather api for Dnipro
+Then weather data should be added
