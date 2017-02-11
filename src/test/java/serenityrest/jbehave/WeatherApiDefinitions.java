e51229806bf59f426f6038bf5091a94e290e981e
alexromanov <al8x.romanov@gmail.com>
2017-02-11 20:11:54 +0200

    Added few scenarios for current weather story

--- src/test/java/serenityrest/jbehave/WeatherApiDefinitions.java
+++ src/test/java/serenityrest/jbehave/WeatherApiDefinitions.java
@@ -8,7 +8,17 @@ import org.jbehave.core.annotations.When;
 public class WeatherApiDefinitions {
 
     @When("I call weather api for $city")
-    public void callApiForCurrentWeather(){
-        
+    public void callApiForCurrentWeather(String city){
+
+    }
+
+    @When("I call weather api for $city and country $country")
+    public void callApiForCurrentWeather(String city, String country){
+
+    }
+
+    @When("I call weather api for city with $cityId id")
+    public void callApiForCurrentWeatherByCityId(String cityId){
+
     }
 }
--- src/test/resources/stories/weatherapi/CurrentWeather.story
+++ src/test/resources/stories/weatherapi/CurrentWeather.story
@@ -8,3 +8,12 @@ So that I can achieve a business goal
 Scenario: Get current weather data for Dnipro city
 When I call weather api for Dnipro
 Then weather data should be added
+
+Scenario: Get current weather for city and country code
+When I call weather api for Dnipro and country ua
+Then weather data should be added
+
+Scenario: Get current weather by city id
+When I call weather api for city with 2172797 id
+Then weather data should be added
+
