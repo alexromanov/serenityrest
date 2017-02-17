8ea99b5366a2df3b3aa6d121c215559fc85b8f78
Anna <ivahnikanna@gmail.com>
2017-02-17 22:27:05 +0200

    commit from my local branch

--- src/test/java/serenityrest/jbehave/WeatherApiDefinitions.java
+++ src/test/java/serenityrest/jbehave/WeatherApiDefinitions.java
@@ -1,11 +1,18 @@
 package serenityrest.jbehave;
 
+import net.thucydides.core.annotations.Steps;
+import org.jbehave.core.annotations.Then;
 import org.jbehave.core.annotations.When;
+import serenityrest.serenity.GeoApiSteps;
+import serenityrest.serenity.WeatherApiSteps;
 
 /**
  * Created by aleksandr on 11.02.17.
  */
 public class WeatherApiDefinitions {
+
+    @Steps
+    private WeatherApiSteps wheatherCodeSteps;
 
     @When("I call weather api for $city")
     public void callApiForCurrentWeather(String city){
@@ -19,6 +26,11 @@ public class WeatherApiDefinitions {
 
     @When("I call weather api for city with $cityId id")
     public void callApiForCurrentWeatherByCityId(String cityId){
+        wheatherCodeSteps.requestWheatherWithCityID(cityId);
+    }
 
+    @Then("weather data should be added")
+    public void thenDataShouldBeReceived() {
+        wheatherCodeSteps.verifyWheatherResponse();
     }
 }
